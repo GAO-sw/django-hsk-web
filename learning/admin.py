d4b9aadd7de77df7f539857701492080d738b473
gaosw <gaosw@yandex.com>
2025-12-18 12:25:19 +0300

    add some comments

--- learning/admin.py
+++ learning/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from .models import Lesson, TextEntry
 
-
+#写这个内联可以一次录入一整节课，一次保存。不必一次次添加句子然后一次次保存。
 class TextEntryInline(admin.TabularInline):
+    #括号内表示句子会横着一条条排
     model = TextEntry
+    #表示这个表格填的是句子表的数据
     extra = 1  
     fields = ('order', 'text_zh', 'text_pinyin', 'text_ru')
     ordering = ('order',)
 
 @admin.register(Lesson)
+#这行是个装饰器，意思是-下面的这个类用来配置管理lesson这个模型
 class LessonAdmin(admin.ModelAdmin):
     list_display = ('title_zh', 'title_ru', 'created_at')
+    #决定了课程列表都显示哪些列
    
     inlines = [TextEntryInline] 
+    #把之前定义的子类表格塞进了父表页面
+    #正式引文这行代码，在课程编辑页面才能看到下方的句子录入框
     
    
     fieldsets = (
@@ -22,6 +28,7 @@ class LessonAdmin(admin.ModelAdmin):
         ('习题配置 (直接粘贴JSON)', {
             'fields': ('exercise_data',),
             'classes': ('collapse',), 
+            #collapse把这个区域默认隐藏，点击后显示展开
         }),
     )
 
--- learning/models.py
+++ learning/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Lesson(models.Model):
+    #括号内代表--这个类是基类，把当前这个类转换为数据库表
 
     title_zh = models.CharField("标题 (中文)", max_length=100, default="第一课")
     title_ru = models.CharField("标题 (俄语)", max_length=100, default="Урок 1")
@@ -13,17 +14,23 @@ class Lesson(models.Model):
     )
     
     created_at = models.DateTimeField(auto_now_add=True)
+    #时间日期字段，自动打卡机，当这条数据第一次被创建时，自动填入当前时间
 
     def __str__(self):
         return f"{self.title_zh} - {self.title_ru}"
+    #决定了在后台这个对象长什么样
 
     class Meta:
         verbose_name = "课程"
         verbose_name_plural = "课程列表"
+    #给模型起个花名，单数叫什么，复数叫什么
 
 class TextEntry(models.Model):
 
     lesson = models.ForeignKey(Lesson, related_name='texts', on_delete=models.CASCADE)
+    #这是一段子表，括号第一个参数定义了谁是爸爸，
+    #第二个参数用语反向查询-父找子lesson.extx.all()就能找到这课所有的句子
+    #第三格参数--如果我在后台把第一课删除，所有第一课的句子也都会被删除
     
   
     text_zh = models.CharField("课文 (汉字)", max_length=255)
@@ -32,8 +39,10 @@ class TextEntry(models.Model):
     
  
     order = models.PositiveIntegerField("顺序 (数字越小越靠前)", default=0)
+    #用来控制句子显示的先后顺序
 
     class Meta:
         ordering = ['order'] 
         verbose_name = "课文句子"
         verbose_name_plural = "课文句子"
+    #第一行，自动排序
